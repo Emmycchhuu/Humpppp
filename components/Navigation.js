a7cec59f6fbb7be9b579b535aa23c88652e76a23
Emmycchhuu <accbombb@gmail.com>
2025-04-10 08:26:38 +0100

    Update Navigation.js

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,13 +2,13 @@ import Link from 'next/link';
 
 export default function Navigation() {
   return (
-    <nav className="p-4 bg-gray-800 flex justify-between">
-      <h1 className="text-xl font-bold">Humper Flasher</h1>
-      <ul className="flex space-x-4">
-        <li><Link href="/" className="hover:text-blue-400">Home</Link></li>
-        <li><Link href="/flash" className="hover:text-blue-400">Flash</Link></li>
-        <li><Link href="/donate" className="hover:text-blue-400">Donate</Link></li>
+    <nav>
+      <h1>Humper Flasher</h1>
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/flash">Flash</Link></li>
+        <li><Link href="/donate">Donate</Link></li>
       </ul>
     </nav>
   );
-}
+    }
