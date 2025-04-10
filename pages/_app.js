c87889a6441a123e0e894d1cee9753a21e22685c
Emmycchhuu <accbombb@gmail.com>
2025-04-10 08:29:26 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../public/styles.css';
+import Navigation from '../components/Navigation';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+  return (
+    <>
+      <Navigation />
+      <Component {...pageProps} />
+    </>
+  );
+    }
